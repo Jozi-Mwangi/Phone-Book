42aa158ab2775c89e30f144c9f00098983675fc1
Jozi-Mwangi <mwasjozi@gmail.com>
2023-02-12 19:07:06 +0300

    2nd commit

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,9 @@ from .import views
 
 urlpatterns = [
     path('',views.main_view, name='main'),
-    path('contacts',views.contacts_view, name='contacts'),
     path('search',views.search_view, name='search'),
     path('create-contact',views.create_contact, name='create-contact'),
 
+    path('contact/<str:pk>',views.contact_view, name='contact'),
     path('update-contact/<str:pk>',views.update_contact, name='update-contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import Contact, Person
+from django.urls import reverse
 from django.db.models import Q
+from .models import Contact, Person
 from .forms import ContactForm
 
 # Create your views here.
 def main_view(request):
-    return render(request, 'main.html')
-
-
-def contacts_view(request):
     contacts = Contact.objects.all()
-    context = {'contacts':contacts}
-    # print(contacts)
-    return render (request, 'contacts.html', context)
+    context={'contacts':contacts}
+    return render(request, 'main.html', context)
+
 
 def search_view (request):
     if request.method == 'GET':
@@ -34,8 +31,12 @@ def create_contact(request):
 
     if request.method == 'POST':
         form =ContactForm(request.POST)
+        name=request.POST['name']
+        phone_number=request.POST['phone_number']
+        email=request.POST['email']
+
         if form.is_valid():
-            contact = form.save(commit=False)
+            contact = Contact(name=name, phone_number=phone_number, email=email)
             contact.save()
             return redirect ('main')
         else:
@@ -51,7 +52,16 @@ def update_contact(request, pk):
 
     if request.method == 'POST':
         form = ContactForm(request.POST, instance=contact)
+        new_name=request.POST['name']
+        new_phone_num=request.POST['phone_number']
+        new_email=request.POST['email']
+
+
         if form.is_valid():
+            contact.name=new_name
+            contact.phone_number=new_phone_num
+            contact.email=new_email
+            contact.save()
             form.save()
             return redirect ('main')
         else:
@@ -62,7 +72,16 @@ def update_contact(request, pk):
     #     form.delete()
     #     return redirect ('main')
     
-    context = {'contact':contact, 'form':form}
+    context = { 'contact':contact,'form':form}
     return render(request, 'update_contact.html', context)
         
-    
+def delete_contact(request, pk):
+    contact = Contact.objects.get(id=pk)
+    contact.delete()
+    return (request, reverse('main'))
+
+def contact_view(request,pk):
+    contact=Contact.objects.get(id=pk)
+
+    context={'contact':contact}
+    return render (request, 'contacts.html', context)
